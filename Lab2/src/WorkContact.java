1804acf28e770306a73d6fdabae6fe887bb89f0d
Connor Stack <Connor Stack@DESKTOP-H7F119K.mshome.net>
2022-09-13 23:08:19 -0500

    Connecting remaining files

---
+++ Lab2/src/WorkContact.java
@@ -0,0 +1,8 @@
+
+public class WorkContact extends Contact{
+	private String title;
+	public WorkContact(String name, String phoneNumber, String title) {
+		super(name, phoneNumber);
+		this.title = title;
+	}
+}
