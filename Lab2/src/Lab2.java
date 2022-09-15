55ac8ec885b02c42ae31fa085fc888b1934d2598
Connor Stack <60162579+ConnorStack@users.noreply.github.com>
2022-09-15 17:32:59 -0500

    Update Lab2.java

--- Lab2/src/Lab2.java
+++ Lab2/src/Lab2.java
@@ -1,7 +1,14 @@
 
 public class Lab2 {
 	public static void main( String[] args ) {
-
+		AddressBook addB = new AddressBook("Family");
+		AddressBook addB2 = new AddressBook("Work Contacts");
+		
+		String path = "C:\\Users\\Connor Stack\\ClassLabs\\lab2_InputOutput\\data\\family.csv";
+		String path2 = "data/work.csv";
+		
+		addB.loadContacts(path);
+		addB2.loadContacts(path2);
 		// Reminder: include a try/catch statement around your code in loadContacts
 //		
 //		AddressBook familyBook = new AddressBook( "Family" );
