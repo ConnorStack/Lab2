f3d4098eda294c087f9f54aeb67bce0f07593320
Connor Stack <60162579+ConnorStack@users.noreply.github.com>
2022-09-15 17:31:04 -0500

    Update AddressBook.java

--- Lab2/src/AddressBook.java
+++ Lab2/src/AddressBook.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 public class AddressBook {
 	private String book;
 	ArrayList<Contact>contacts;
+	
+	public AddressBook(String book) {
+		this.book = book;
+	}
+	
 	public String toString() {
 		return "";
 	}
@@ -10,8 +15,35 @@ public class AddressBook {
 	public void addContact(Contact contact) {
 		
 	}
-	public void loadContacts(){
+	
+	public void loadContacts(String filename) { 
+		Scanner scan = null;
+		Contact temp = null;
 		
+		//System.out.println("in loadContact...book is: "+ book);//debug DELETE LATER
+		try{
+			scan = new Scanner(new File(filename));	
+			System.out.println("filename is: " + filename);//debug DELETE LATER
+			while(scan.hasNext()) {
+				//System.out.println("in loadContact while...");//ddebug DELETE LATER
+				String record = scan.nextLine();
+				String[] data = record.split(",");
+				if(this.book.equals("Family")) {
+					temp = new FamilyMember(data[0], data[1], data[2], data[3]);
+					System.out.println("if statement");//debug DELETE LATER
+					contacts.add(temp);
+					
+				}else if(this.book.equals("Work Contacts")) {
+					temp = new WorkContact(data[0], data[1], data[2]);
+					contacts.add(temp);
+				}
+				//System.out.println(data[0] + data[1] + data[2] + data[3]);debug DELETE LATER
+			}
+			scan.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 			
 }
